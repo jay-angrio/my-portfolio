1a2c62c267787a29e38ed842b423d97465541868
jay-angrio <jayangrio31@gmail.com>
2024-05-21 18:07:41 +0530

    my portfolio

---
+++ src/components/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./App.css";
+import SocialLinks from "./SocialLinks";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white about-upper"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+
+        <p className="text-xl mt-20">
+          Hello! I'm Baldaniya Jay B., an aspiring Angular Developer currently
+          pursuing my B.Tech in Information Technology. With a solid foundation
+          in web development technologies, I am passionate about creating
+          dynamic and responsive web applications.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          As a B.Tech IT student, I have dedicated myself to mastering the tools
+          and technologies essential for modern web development. My projects
+          reflect my commitment to learning and my ability to apply theoretical
+          knowledge to practical scenarios.
+        </p>
+
+        <br />
+
+        <p className="text-xl">
+          I am driven by a passion for continuous learning and innovation. My
+          goal is to leverage my skills in Angular and other technologies to
+          contribute to impactful projects and grow as a developer. I am excited
+          about the opportunities to collaborate with like-minded professionals
+          and make meaningful contributions to the tech community.
+        </p>
+      </div>
+      <SocialLinks />
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import "./App.css";
+import securedochub from "../assets/portfolio/securedochub.png";
+import allukbuilder from "../assets/portfolio/allukbuilder.png";
+import saidevelopers from "../assets/portfolio/saidevelopers.png";
+import { BsBoxArrowUpRight } from "react-icons/bs";
+
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 1,
+      src: securedochub,
+      url: "https://dochub-e5625.web.app/",
+    },
+    {
+      id: 2,
+      src: allukbuilder,
+      url: "https://www.myukbuilders.co.uk/",
+    },
+    {
+      id: 3,
+      src: saidevelopers,
+      url: "https://saidevelopers-website.web.app/index.html",
+    },
+  ];
+
+  const openWebsite = (url) => {
+    window.open(url, "_blank");
+  };
+
+  return (
+    <div
+      name="projects"
+      className="bg-gradient-to-b from-black to-gray-800 w-full text-white project-space md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Projects
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, url }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt=""
+                className="rounded-md duration-200 hover:scale-105"
+              />
+              <div className="flex items-center justify-center">
+                <button
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 flex items-center justify-center"
+                  onClick={() => openWebsite(url)}
+                >
+                  Site
+                  <span className="px-3">
+                    <BsBoxArrowUpRight />
+                  </span>
+                </button>
+                {/* <button className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                  Code
+                </button> */}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
